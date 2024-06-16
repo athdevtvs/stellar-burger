1667ba6b70d32be126903f4fc79ce31228172576
Tatiana Vaganova <athdev@yandex.ru>
2024-06-16 17:16:47 +0300

    feat/add the routes with stubs for mandatory options within the app file; initial commit of the ProtectedRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,110 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import {
+  ConstructorPage,
+  NotFound404,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders
+} from '@pages';
+import { AppHeader, ProtectedRoute, Modal, OrderInfo } from '@components';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  const handleCloseFeed = () => {};
+
+  return (
+    <>
+      <div className={styles.app}>
+        <AppHeader />
+        <Routes>
+          <Route path='*' element={<NotFound404 />} />
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route
+            path='/login'
+            element={
+              <ProtectedRoute>
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <ProtectedRoute>
+                <Register />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/forgot-password'
+            element={
+              <ProtectedRoute>
+                <ForgotPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/reset-password'
+            element={
+              <ProtectedRoute>
+                <ResetPassword />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile'
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/profile/orders'
+            element={
+              <ProtectedRoute>
+                <ProfileOrders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='/feed/:number' onClose={handleCloseFeed}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='/ingredients/:id' onClose={handleCloseFeed}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute>
+                <Modal title='/ingredients/:id' onClose={handleCloseFeed}>
+                  <OrderInfo />
+                </Modal>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { /*Route, Redirect,*/ RouteProps } from 'react-router-dom';
+
+export const ProtectedRoute: FC<RouteProps> = ({ children, path, ...rest }) => {
+  let test = 0;
+
+  return null;
+};
